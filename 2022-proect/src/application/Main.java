640637c5b916ac1e082d7ad2a833cc4eb1978043
Ber-Vazq <bvaq20@hotmail.com>
2022-04-20 20:14:14 -0500

    extra changes whoops

--- 2022-proect/src/application/Main.java
+++ 2022-proect/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("application/view/mainMenu.fxml"));
-			Scene scene = new Scene(root,579,429);
+			Scene scene = new Scene(root,600,429);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
